0a14b88006eaf1d5dda6614a94876d6eee6a18b3
Alexander.Fernandez <63027068+alexjose131@users.noreply.github.com>
2020-05-02 15:00:59 -0400

    FASE1-073 Se agregó el modulo de consulta de parametros generales (#1)
    * FASE1-073 Se agregó el modulo de consulta de parametros generales
    
    * solucionado el error de logger y configurada la estructura con eslint y prettier
    
    * FASE1-073: modificado mensaje de error del controlador

--- modules/setting/setting.controller.js
+++ modules/setting/setting.controller.js
@@ -2,4 +2,20 @@ const createError = require("http-errors");
 const settingModel = require("./setting.model");
 const logger = require("../../config/logLevels");
 
-module.exports = {};
+module.exports = {
+  getAllSettings: async (req, res, next) => {
+    let result = await settingModel.getAllSettings(req.con);
+    if (result instanceof Error) {
+      logger.error("Error en el modulo Setting (getAllSettings)");
+      next(
+        createError(
+          500,
+          "Error al obtener los parametros globales (" + result.message + ")"
+        )
+      );
+    } else {
+      logger.info("Lista de configuraciones entregada");
+      res.json(result);
+    }
+  },
+};
