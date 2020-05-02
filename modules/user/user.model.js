9f2aef91a63007ef56c28de33687b5ce41db7bdd
Diego De Quintal <dadequintal.17@est.ucab.edu.ve>
2020-05-02 19:43:48 -0400

    Se modifico el query a letras mayusculas

--- modules/user/user.model.js
+++ modules/user/user.model.js
@@ -1,22 +1,19 @@
 module.exports = {
   getAllUsers: (con) => {
-    return con
-      .query("SELECT * FROM EC_USER where \
-      user_first_name='PEPE' ")
-      .catch((error) => {
-        return new Error(error);
-      });
+    return con.query("SELECT * FROM EC_USER").catch((error) => {
+      return new Error(error);
+    });
   },
 
   getShoppingCart: (con) => {
     return con
       .query(
-        "select ppo.*, p.product_name ,  pr.provider_name \
-        from ec_product_provider_order ppo, ec_product p, ec_provider pr, ec_product_provider pp\
-        where ppo.fk_product_provider_id = pp.product_provider_id \
-        and pp.fk_product_id = p.product_id \
-        and pp.fk_provider_id = pr.provider_id \
-        and ppo.fk_status_id in ('1','2')"
+        "SELECT PPO.*, P.product_name ,  PR.provider_name \
+        FROM EC_PRODUCT_PROVIDER_ORDER PPO, EC_PRODUCT P, EC_PROVIDER PR, EC_PRODUCT_PROVIDER PP\
+        WHERE PPO.fk_product_provider_id = PP.product_provider_id \
+        AND PP.fk_product_id = P.product_id \
+        AND PP.fk_provider_id = PR.provider_id \
+        AND PPO.fk_status_id in ('1','2')"
       )
       .catch((error) => {
         return new Error(error);
