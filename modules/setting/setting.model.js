7e94b902aafab0ddcc3916f4ccd76ab84196a235
Alexander.Fernandez <63027068+alexjose131@users.noreply.github.com>
2020-05-03 13:13:16 -0400

    Fase1 072 (#3)
    * FASE-072: se agrega actualizar los parametros de configuracion al modulo setting
    
    * FASE1-072: modificadas las sentencias sql, eliminado la llamada al middleware de snackcase y se hace uso de las propiedades de la respuesta
    
    * modificada la respuesta al actualizar de manera efectiva

--- modules/setting/setting.model.js
+++ modules/setting/setting.model.js
@@ -4,4 +4,15 @@ module.exports = {
       return new Error(error);
     });
   },
+
+  updateSettings: (con, body) => {
+    return con.query(
+      "UPDATE EC_SETTING SET setting_payment_processor = $2, setting_service_commission = $3 WHERE setting_id = $1",
+      [
+        body.setting_id,
+        body.setting_payment_processor,
+        body.setting_service_commission,
+      ]
+    );
+  },
 };
--- modules/setting/setting.router.js
+++ modules/setting/setting.router.js
@@ -4,5 +4,5 @@ const settingController = require("./setting.controller");
 const auth = require("../../middlewares/auth");
 
 router.get("/", auth.validateToken, settingController.getAllSettings);
-
+router.put("/", auth.validateToken, settingController.updateSettings);
 module.exports = router;
