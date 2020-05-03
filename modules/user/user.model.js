afb4d679b9c2afbfa3343b3d89a45f2d2cbeed89
diegodequintal <63262264+diegodequintal@users.noreply.github.com>
2020-05-02 20:00:31 -0400

    FASE1-021 (#2)
    * FASE1-021 Se agregó la funcionalidad para obtener el carrito de compras de un usuario
    
    * Se modifico el query a letras mayusculas

--- modules/user/user.model.js
+++ modules/user/user.model.js
@@ -4,4 +4,19 @@ module.exports = {
       return new Error(error);
     });
   },
+
+  getShoppingCart: (con) => {
+    return con
+      .query(
+        "SELECT PPO.*, P.product_name ,  PR.provider_name \
+        FROM EC_PRODUCT_PROVIDER_ORDER PPO, EC_PRODUCT P, EC_PROVIDER PR, EC_PRODUCT_PROVIDER PP\
+        WHERE PPO.fk_product_provider_id = PP.product_provider_id \
+        AND PP.fk_product_id = P.product_id \
+        AND PP.fk_provider_id = PR.provider_id \
+        AND PPO.fk_status_id in ('1','2')"
+      )
+      .catch((error) => {
+        return new Error(error);
+      });
+  },
 };
