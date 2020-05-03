95fb0c5525e836d1ad4abc3f9f07911bfaf4ee6a
Diego De Quintal <dadequintal.17@est.ucab.edu.ve>
2020-05-02 22:18:36 -0400

    FASE1-021 Se agrego una mejora al query para obtener los productos del carrito

--- modules/user/user.model.js
+++ modules/user/user.model.js
@@ -8,12 +8,13 @@ module.exports = {
   getShoppingCart: (con) => {
     return con
       .query(
-        "SELECT PPO.*, P.product_name ,  PR.provider_name \
-        FROM EC_PRODUCT_PROVIDER_ORDER PPO, EC_PRODUCT P, EC_PROVIDER PR, EC_PRODUCT_PROVIDER PP\
-        WHERE PPO.fk_product_provider_id = PP.product_provider_id \
-        AND PP.fk_product_id = P.product_id \
-        AND PP.fk_provider_id = PR.provider_id \
-        AND PPO.fk_status_id in ('1','2')"
+        `SELECT PPO.*, P.product_name ,  PR.provider_name, PP.PRODUCT_PROVIDER_PRICE, 
+        (PP.PRODUCT_PROVIDER_PRICE * PPO.PRODUCT_PROVIDER_ORDER_QUANTITY) AS TOTAL 
+        FROM EC_PRODUCT_PROVIDER_ORDER PPO, EC_PRODUCT P, EC_PROVIDER PR, EC_PRODUCT_PROVIDER PP
+        WHERE PPO.fk_product_provider_id = PP.product_provider_id
+        AND PP.fk_product_id = P.product_id
+        AND PP.fk_provider_id = PR.provider_id
+        AND PPO.fk_status_id in ('1','2')`
       )
       .catch((error) => {
         return new Error(error);
