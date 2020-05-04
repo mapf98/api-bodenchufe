d54e8ff59c9505d3706af5ac3c0a00a6a24ef288
alexjose131 <ajfernandez.17@est.ucab.edu.ve>
2020-05-04 11:03:20 -0400

    se agrega el catch en el el model de category

--- modules/category/category.model.js
+++ modules/category/category.model.js
@@ -1,6 +1,12 @@
 module.exports = {
   updateCategory: (con, body) => {
-    con.query(`UPDATE EC_CATEGORY SET category_name = '${body.category_name}'
-               WHERE category_id = ${body.category_id}`);
+    con
+      .query(
+        `UPDATE EC_CATEGORY SET category_name = '${body.category_name}'
+               WHERE category_id = ${body.category_id}`
+      )
+      .catch((error) => {
+        return new Error(error);
+      });
   },
 };
