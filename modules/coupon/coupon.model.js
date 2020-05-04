b858ac1ccdc7062c0434914cd69ea328a1aaec33
alexjose131 <ajfernandez.17@est.ucab.edu.ve>
2020-05-04 03:35:21 -0400

    FASE1-064: Se agrega la funcionalidad de agregar cupones en el modulo coupon

--- modules/coupon/coupon.model.js
+++ modules/coupon/coupon.model.js
@@ -4,4 +4,15 @@ module.exports = {
       return new Error(error);
     });
   },
+
+  addCoupon: (con, body) => {
+    return con
+      .query(
+        `INSERT INTO EC_COUPON (coupon_name, coupon_discount_rate, fk_status_id, fk_user_id) 
+                      VALUES ('${body.coupon_name}', '${body.coupon_discount_rate}', 3 , ${body.fk_user_id})`
+      )
+      .catch((error) => {
+        return new Error(error);
+      });
+  },
 };
--- modules/coupon/coupon.router.js
+++ modules/coupon/coupon.router.js
@@ -4,5 +4,6 @@ const couponController = require("./coupon.controller");
 const auth = require("../../middlewares/auth");
 
 router.get("/", auth.validateToken, couponController.getCoupons);
+router.post("/", auth.validateToken, couponController.addCoupon);
 
 module.exports = router;
