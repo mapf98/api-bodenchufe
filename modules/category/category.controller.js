437c5c322c1cb38a8cfa90fbb587313644beca0d
alexjose131 <ajfernandez.17@est.ucab.edu.ve>
2020-05-04 11:34:17 -0400

    FASE1-067: se ha agregado la creacion de una categoria

--- modules/category/category.controller.js
+++ modules/category/category.controller.js
@@ -2,4 +2,21 @@ const createError = require("http-errors");
 const categoryModel = require("./category.model");
 const logger = require("../../config/logLevels");
 
-module.exports = {};
+module.exports = {
+  createCategory: async (req, res, next) => {
+    let result = await categoryModel.createCategory(req.con, req.body);
+
+    if (result instanceof Error) {
+      logger.error("Error en el modulo category (createCategory)");
+      next(
+        createError(
+          500,
+          `Ha ocurrido un error al crear la categoria (${result.message})`
+        )
+      );
+    } else {
+      logger.info("La categoria se ha creado satisfactoriamente");
+      res.json({ status: 200 });
+    }
+  },
+};
--- modules/category/category.router.js
+++ modules/category/category.router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const category = require("./category.controller");
+const categoryController = require("./category.controller");
 const auth = require("../../middlewares/auth");
+
+router.post("/", auth.validateToken, categoryController.createCategory);
 
 module.exports = router;
