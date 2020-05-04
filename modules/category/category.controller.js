a22183437557b4264041bc6c7b1dcec31044dab4
alexjose131 <ajfernandez.17@est.ucab.edu.ve>
2020-05-04 01:21:22 -0400

    FASE1-004: Se agrega la funcionalidad de obtener las categorias padres

--- modules/category/category.controller.js
+++ modules/category/category.controller.js
@@ -2,4 +2,21 @@ const createError = require("http-errors");
 const categoryModel = require("./category.model");
 const logger = require("../../config/logLevels");
 
-module.exports = {};
+module.exports = {
+  getMainCategories: async (req, res, next) => {
+    let results = await categoryModel.getMainCategories(req.con);
+
+    if (results instanceof Error) {
+      logger.error("Error en el modulo category (getMainCategories)");
+      next(
+        createError(
+          500,
+          `Error al obtener las categorias padres (${results.message})`
+        )
+      );
+    } else {
+      logger.info("Lista de categorias padre obtenida satisfactoriamente");
+      res.json(results);
+    }
+  },
+};
--- modules/category/category.model.js
+++ modules/category/category.model.js
@@ -1,1 +1,9 @@
-module.exports = {};
+module.exports = {
+  getMainCategories: (con) => {
+    return con
+      .query("SELECT * FROM EC_CATEGORY WHERE fk_category_id IS NULL")
+      .catch((error) => {
+        return new Error(error);
+      });
+  },
+};
