330fa6a6364cbc4ba84efc0aa62b575dc2a1a865
alexjose131 <ajfernandez.17@est.ucab.edu.ve>
2020-05-04 02:58:23 -0400

    FASE1-065: agregado deshabilitar cupon en el modulo coupon

--- modules/coupon/coupon.model.js
+++ modules/coupon/coupon.model.js
@@ -4,4 +4,19 @@ module.exports = {
       return new Error(error);
     });
   },
+
+  disableCoupon: (con, body) => {
+    console.log(body);
+    return con
+      .query(
+        `UPDATE EC_COUPON 
+         SET fk_status_id = (SELECT status_id 
+                             FROM EC_STATUS 
+                             WHERE status_name = '${body.statusName}') 
+         WHERE coupon_id = ${body.coupon_id}`
+      )
+      .catch((error) => {
+        return new Error(error);
+      });
+  },
 };
