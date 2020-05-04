9be32654c7e6158cddc79893061d0e5f2db52a2e
mapf98 <46854096+mapf98@users.noreply.github.com>
2020-05-04 11:42:28 -0400

    Se obtienen productos en base a una categoría (#11)

--- modules/product/product.router.js
+++ modules/product/product.router.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const productController = require("./product.controller");
 const auth = require("../../middlewares/auth");
 
+router.use("/category/:categoryId", productController.getProductsByCategory);
+
 module.exports = router;
