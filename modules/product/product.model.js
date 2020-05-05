0c2ac46413716fc40c1a68723466f563434b0904
Miguel Antonio Peña Fraga <mapena.16@est.ucab.edu.ve>
2020-05-04 23:51:20 -0400

    Se arregló la consulta de productos en base a una offerta

--- modules/product/product.model.js
+++ modules/product/product.model.js
@@ -41,9 +41,35 @@ module.exports = {
   getProductsByOffer: (con, offer_id) => {
     return con
       .query(
-        `SELECT * FROM EC_PRODUCT AS PRO, EC_OFFER AS OFR 
-          WHERE PRO.fk_offer_id = OFR.offer_id
-            AND OFR.offer_id = ${offer_id}`
+        `SELECT PP.product_provider_id AS post_id,
+                PRO.product_id,
+                PRO.product_name,
+                PRO.product_photo,
+                PRO.product_long,
+                PRO.product_height,
+                PRO.product_width,
+                CAT.category_name,
+                PVD.provider_name,
+                PP.product_provider_price,
+                PP.product_provider_available_quantity,
+                (SELECT AVG(QUAUX.qualification_stars) 
+                FROM EC_QUALIFICATION AS QUAUX
+                WHERE QUAUX.fk_product_provider_id = PP.product_provider_id) AS avg_qualification_stars,
+                (SELECT OFRAUX.offer_rate
+                FROM EC_OFFER AS OFRAUX,
+                  EC_PRODUCT_PROVIDER AS PPAUX
+                WHERE OFRAUX.offer_id = PPAUX.fk_offer_id
+                  AND PPAUX.product_provider_id = PP.product_provider_id) AS offer_rate
+          FROM EC_PRODUCT_PROVIDER AS PP,
+            EC_PRODUCT AS PRO,
+            EC_PROVIDER AS PVD,
+            EC_STATUS AS STA,
+            EC_CATEGORY AS CAT
+          WHERE PP.fk_product_id = PRO.product_id 
+            AND PP.fk_provider_id = PVD.provider_id
+            AND STA.status_id = PP.fk_status_id
+            AND CAT.category_id = PRO.fk_category_id
+            AND PP.fk_offer_id = ${offer_id}`
       )
       .catch((error) => {
         return new Error(error);
