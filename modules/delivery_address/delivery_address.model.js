9324b603f848f90a6781ca7852134e2a76887c52
Diego De Quintal <dadequintal.17@est.ucab.edu.ve>
2020-05-05 14:42:31 -0400

    FASE1-025 Se agrego la consulta de las direcciones de entrega de los usuarios al igual que todas las direcciones de los usuarios

--- modules/delivery_address/delivery_address.model.js
+++ modules/delivery_address/delivery_address.model.js
@@ -1,1 +1,19 @@
-module.exports = {};
+module.exports = {
+  getDeliveryAddresses: (req) => {
+    if (req.originalUrl === "/bodenchufe/api/delivery-address") {
+      return req.con
+        .query(`SELECT * FROM EC_DELIVERY_ADDRESS`)
+        .catch((error) => {
+          return new Error(error);
+        });
+    } else if (req.originalUrl === "/bodenchufe/api/user/deliveryAddress") {
+      return req.con
+        .query(
+          `SELECT * FROM EC_DELIVERY_ADDRESS WHERE FK_USER_ID = ${req.user_id}`
+        )
+        .catch((error) => {
+          return new Error(error);
+        });
+    }
+  },
+};
