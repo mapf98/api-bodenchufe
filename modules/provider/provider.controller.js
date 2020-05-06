19911535821375a0a84e0eb688522866ec6bb4e6
mapf98 <46854096+mapf98@users.noreply.github.com>
2020-05-05 23:17:44 -0400

    FASE1-055 Obtener proveedores y sus publicaciones (productos) asociados (#31)
    * Consulta de todas las ordenes dentro del sistema para BackOffice
    
    * Consulta de proveedores y productos asociados para el administrador

--- modules/provider/provider.controller.js
+++ modules/provider/provider.controller.js
@@ -3,11 +3,11 @@ const providerModel = require("./provider.model");
 const logger = require("../../config/logLevels");
 
 module.exports = {
-  getAllProviders: async (req, res, next) => {
-    let providers = await providerModel.getAllProviders(req.con);
+  getMainProviders: async (req, res, next) => {
+    let providers = await providerModel.getMainProviders(req.con);
 
     if (providers instanceof Error) {
-      logger.error("Error en el modulo provider (getAllProviders)");
+      logger.error("Error en el modulo provider (getMainProviders)");
       next(
         createError(
           500,
@@ -18,6 +18,36 @@ module.exports = {
       logger.info("Lista de proveedores obtenida satisfactoriamente");
       res.json(providers);
     }
+  },
+  getAllProviders: async (req, res, next) => {
+    let providersResume = [];
+    let providers = await providerModel.getAllProviders(req.con);
+
+    for (let index = 0; index < providers.length; index++) {
+      let products = await providerModel.getProvidersProducts(
+        req.con,
+        providers[index].provider_id
+      );
+      providersResume.push({
+        provider: providers[index],
+        posts: products,
+      });
+    }
+
+    if (providers instanceof Error) {
+      logger.error("Error en el modulo provider (getAllProviders)");
+      next(
+        createError(
+          500,
+          `Error al obtener los proveedores para el admin (${providers.message})`
+        )
+      );
+    } else {
+      logger.info(
+        "Lista de proveedores para admin obtenida satisfactoriamente"
+      );
+      res.json(providersResume);
+      }
   },
   createProvider: async (req, res, next) => {
     let provider = await providerModel.createProvider(req.con, req.body);
--- modules/provider/provider.model.js
+++ modules/provider/provider.model.js
@@ -1,8 +1,52 @@
 module.exports = {
-  getAllProviders: (con) => {
+  getMainProviders: (con) => {
     return con.query("SELECT * FROM EC_PROVIDER").catch((error) => {
       return new Error(error);
     });
+  },
+  getAllProviders: (con) => {
+    return con
+      .query(
+        `SELECT PRV.provider_id,	
+                PRV.provider_name,
+                PRV.provider_description,
+                STA.status_name,
+                (SELECT COUNT(*)
+                FROM EC_PRODUCT_PROVIDER AS PPV,
+                    EC_PROVIDER AS PRVAUX
+                WHERE PPV.fk_provider_id = PRVAUX.provider_id
+                    AND PRVAUX.provider_id = PRV.provider_id) AS provider_posts
+          FROM EC_PROVIDER AS PRV,
+          EC_STATUS AS STA
+          WHERE PRV.fk_status_id = STA.status_id`
+      )
+      .catch((error) => {
+        return new Error(error);
+      });
+  },
+  getProvidersProducts: (con, providerId) => {
+    return con
+      .query(
+        `SELECT PRO.product_photo,
+                  PRO.product_name,
+                  PPV.product_provider_available_quantity,
+                  PPV.product_provider_price,
+                  PPV.product_provider_description,
+                  (SELECT OFRAUX.offer_rate
+                  FROM EC_OFFER AS OFRAUX,
+                      EC_PRODUCT_PROVIDER AS PPVAUX
+                  WHERE OFRAUX.offer_id = PPVAUX.fk_offer_id
+                      AND PPVAUX.product_provider_id = PPV.product_provider_id) AS product_discount_rate
+          FROM EC_PRODUCT_PROVIDER AS PPV,
+              EC_PRODUCT AS PRO,
+              EC_PROVIDER AS PVD
+          WHERE PPV.fk_product_id = PRO.product_id
+              AND PPV.fk_provider_id = PVD.provider_id
+              AND PVD.provider_id = ${providerId}`
+      )
+      .catch((error) => {
+        return new Error(error);
+      });
   },
   createProvider: (con, provider) => {
     return con
