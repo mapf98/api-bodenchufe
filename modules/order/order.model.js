bc8530f39de03a06e6d434be1f1d78cf14d461e0
mapf98 <46854096+mapf98@users.noreply.github.com>
2020-05-05 22:41:21 -0400

    FASE1-054 Agregar un nuevo proveedor, arreglos menores en los módulos de order y authentication. (#30)
    * Consulta de todas las ordenes dentro del sistema para BackOffice
    
    * Se agregó la creación de un proveedor desde el Backoffice
    
    * Se agregó validación de token a GET Orders
    
    * Se quitó un console log
    
    * Arreglos en la obtención de ordenes

--- modules/order/order.model.js
+++ modules/order/order.model.js
@@ -12,9 +12,16 @@ module.exports = {
                   DAS.delivery_address_primary_line,
                   (SELECT COUAUX.coupon_discount_rate
                     FROM EC_COUPON AS COUAUX,
-                        EC_ORDER AS ORDAUX
+                          EC_ORDER AS ORDAUX
                     WHERE COUAUX.coupon_id = ORDAUX.fk_coupon_id
-                        AND ORDAUX.order_id = ORD.order_id) AS coupon_applied
+                            AND ORDAUX.order_id = ORD.order_id) AS coupon_applied,
+                  (SELECT DISTINCT USR.user_email
+                    FROM EC_USER AS USR,
+                          EC_PRODUCT_PROVIDER_ORDER AS PPO,
+                          EC_ORDER AS ORDAUX
+                    WHERE USR.user_id = PPO.fk_user_id
+                            AND ORDAUX.order_id = PPO.fk_order_id
+                            AND ORDAUX.order_id = ORD.order_id) AS user_email
           FROM EC_ORDER AS ORD,
               EC_DELIVERY_ADDRESS AS DAS,
               EC_STATUS AS STA
@@ -28,24 +35,21 @@ module.exports = {
   getAllProductsOrder: (con, orderId) => {
     return con
       .query(
-        `SELECT USR.user_email,
-                  PPO.product_provider_order_quantity AS product_quantity,
-                  PPV.product_provider_price AS product_price,
-                  PRV.provider_name,
-                  PRO.product_name,
-                  PRO.product_photo
+        `SELECT PPO.product_provider_order_quantity AS product_quantity,
+                PPV.product_provider_price AS product_price,
+                PRV.provider_name,
+                PRO.product_name,
+                PRO.product_photo
           FROM EC_PRODUCT_PROVIDER_ORDER AS PPO,
-              EC_ORDER AS ORD,
-              EC_USER AS USR,
-              EC_PRODUCT_PROVIDER AS PPV,
-              EC_PROVIDER AS PRV,
-              EC_PRODUCT AS PRO
+                EC_ORDER AS ORD,
+                EC_PRODUCT_PROVIDER AS PPV,
+                EC_PROVIDER AS PRV,
+                EC_PRODUCT AS PRO
           WHERE PPO.fk_order_id = ORD.order_id
-              AND PPO.fk_user_id = USR.user_id
-              AND PPO.fk_product_provider_id = PPV.product_provider_id
-              AND PPV.fk_provider_id = PRV.provider_id
-              AND PPV.fk_product_id = PRO.product_id
-              AND ORD.order_id = ${orderId}`
+                AND PPO.fk_product_provider_id = PPV.product_provider_id
+                AND PPV.fk_provider_id = PRV.provider_id
+                AND PPV.fk_product_id = PRO.product_id
+                AND ORD.order_id = ${orderId}`
       )
       .catch((error) => {
         return new Error(error);
--- modules/provider/provider.model.js
+++ modules/provider/provider.model.js
@@ -4,4 +4,14 @@ module.exports = {
       return new Error(error);
     });
   },
+  createProvider: (con, provider) => {
+    return con
+      .query(
+        `INSERT INTO EC_PROVIDER (provider_name, provider_description, fk_status_id) 
+          VALUES ('${provider.provider_name}', '${provider.provider_description}', (SELECT status_id FROM EC_STATUS WHERE status_name = 'active'))`
+      )
+      .catch((error) => {
+        return new Error(error);
+      });
+  },
 };
