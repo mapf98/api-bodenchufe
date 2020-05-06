918dc36859405d981c4a162c5ebfff0e0da441f1
mapf98 <46854096+mapf98@users.noreply.github.com>
2020-05-06 10:38:31 -0400

    FASE1-060 Actualización de productos asociados a un proveedor (#37)
    * Consulta de todas las ordenes dentro del sistema para BackOffice
    
    * Se realizó la uri para la actualización de un proveedor
    
    * Cambio de put a patch
    
    * Actualización de información de proveedores
    
    * Se realizó la uri para efectuar cambios en los productos de un proveedor (publicaciones)

--- modules/provider/provider.model.js
+++ modules/provider/provider.model.js
@@ -81,4 +81,18 @@ module.exports = {
         return new Error(error);
       });
   },
+  updateProductProvider: (con, post) => {
+    return con
+      .result(
+        `UPDATE EC_PRODUCT_PROVIDER
+          SET product_provider_description = '${post.description}',
+            product_provider_price = ${post.price},
+            product_provider_available_quantity = ${post.available_quantity},	
+            fk_offer_id = (SELECT offer_id FROM EC_OFFER AS OFR WHERE OFR.offer_rate = '${post.offer_rate}') 
+          WHERE product_provider_id = ${post.post_id}`
+      )
+      .catch((error) => {
+        return new Error(error);
+      });
+  },
 };
