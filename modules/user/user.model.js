2d3f017aa511a87a8d0de62a2b9ddb25060b588c
Alexander.Fernandez <63027068+alexjose131@users.noreply.github.com>
2020-05-05 23:33:50 -0400

    Fase1 027: Editar dirección de entrega (#32)
    * FASE1-027: se agrega la funcionalidad de editar una direccion de correo
    
    * cambios en el response

--- modules/user/user.model.js
+++ modules/user/user.model.js
@@ -87,4 +87,15 @@ module.exports = {
         return new Error(error);
       });
   },
+  updateDeliveryAddress: (con, body, params) => {
+    return con
+      .query(
+        `UPDATE EC_DELIVERY_ADDRESS 
+         SET delivery_address_primary_line = '${body.delivery_address_primary_line}', delivery_address_secondary_line = '${body.delivery_address_secondary_line}', delivery_address_city = '${body.delivery_address_city}', delivery_address_state ='${body.delivery_address_state}', delivery_address_zip_code = ${body.delivery_address_zip_code}, delivery_address_aditional_info = '${body.delivery_address_aditional_info}', delivery_address_security_code = ${body.delivery_address_security_code}, delivery_address_locker_code = ${body.delivery_address_locker_code}
+         WHERE delivery_address_id = ${params.deliveryAddressId}`
+      )
+      .catch((error) => {
+        return new Error(error);
+      });
+  },
 };
