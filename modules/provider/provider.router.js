40322fc1374ec22ca8d1e1335ebe32408c326e09
Miguel Antonio Peña Fraga <mapena.16@est.ucab.edu.ve>
2020-05-05 22:41:51 -0400

    Consulta de proveedores y productos asociados para el administrador

--- modules/provider/provider.router.js
+++ modules/provider/provider.router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const providerController = require("./provider.controller");
 const auth = require("../../middlewares/auth");
 
-router.get("/", providerController.getAllProviders);
+router.get("/main", providerController.getMainProviders);
+router.get("/", auth.validateToken, providerController.getAllProviders);
 
 module.exports = router;
