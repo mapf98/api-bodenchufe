9dc52392fa1ed75a636056e0a13ec633faa24316
diegodequintal <63262264+diegodequintal@users.noreply.github.com>
2020-05-07 02:33:19 -0400

    FASE1-051-052 Se implemento el bloqueo y activacion de cuentas por parte del administrador (#43)

--- modules/user/user.controller.js
+++ modules/user/user.controller.js
@@ -23,19 +23,51 @@ module.exports = {
       data: { users },
     });
   },
-  disableAccount: async (req, res, next) => {
+  disableMyAccount: async (req, res, next) => {
     let user = await userModel.updateStatusAccount(req);
     if (user instanceof Error) {
       logger.error("Error en modulo user (PATCH /disableMe)");
       return next(
         createError(
           500,
-          `Error al desactivar la cuenta del usuario (${users.message})`
+          `Error al desactivar la cuenta del usuario (${user.message})`
         )
       );
     }
 
     logger.info("Cuenta del usuario desactivada");
+    res.json({
+      status: "success",
+    });
+  },
+  activateAccount: async (req, res, next) => {
+    let user = await userModel.activateAccount(req);
+    if (user instanceof Error) {
+      logger.error("Error en modulo user (PATCH /activateAccount/:userId)");
+      return next(
+        createError(
+          500,
+          `Error al activar la cuenta del usuario (${user.message})`
+        )
+      );
+    }
+    logger.info("Cuenta del usuario activada");
+    res.json({
+      status: "success",
+    });
+  },
+  blockAccount: async (req, res, next) => {
+    let user = await userModel.blockAccount(req);
+    if (user instanceof Error) {
+      logger.error("Error en modulo user (PATCH /blockAccount/:userId)");
+      return next(
+        createError(
+          500,
+          `Error al bloquear la cuenta del usuario (${user.message})`
+        )
+      );
+    }
+    logger.info("Cuenta del usuario bloqueada");
     res.json({
       status: "success",
     });
