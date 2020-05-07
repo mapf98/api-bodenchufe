ff5582f990062a53b68bf0dca0801ef20cdbb73c
diegodequintal <63262264+diegodequintal@users.noreply.github.com>
2020-05-07 13:34:29 -0400

    FASE1-ROLES/PRIVILEGIOS Se implemento manejo de roles y privilegios para el acceso a las peticiones (#44)

--- modules/authentication/authentication.model.js
+++ modules/authentication/authentication.model.js
@@ -2,12 +2,15 @@ module.exports = {
   logIn: (con, body) => {
     return con
       .query(
-        `SELECT E.user_id, E.user_first_name, E.user_first_lastname, E.user_second_name, E.user_second_lastname , E.user_birthdate, E.user_email, E.user_password, E.user_photo, E.fk_language_id, E.fk_rol_id, E.fk_status_id, S.status_name , L.language_name
-         FROM EC_USER E, EC_STATUS S, EC_LANGUAGE L
+        `SELECT E.user_id, E.user_first_name, E.user_first_lastname, E.user_second_name, 
+         E.user_second_lastname , E.user_birthdate, E.user_email, E.user_password, E.user_photo,
+         E.fk_language_id, E.fk_rol_id, E.fk_status_id, S.status_name , L.language_name, R.rol_name
+         FROM EC_USER E, EC_STATUS S, EC_LANGUAGE L, EC_ROL R
          WHERE E.user_email= '${body.user_email}' 
          AND E.user_password='${body.user_password}'
          AND E.fk_status_id = S.status_id
-         AND E.fk_language_id = L.language_id`
+         AND E.fk_language_id = L.language_id
+         AND E.fk_rol_id = R.rol_id`
       )
       .catch((error) => {
         return new Error(error);
