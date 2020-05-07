e680e7a06708c11f6e8fe31ba7f78fae970d262d
Miguel Antonio Peña Fraga <mapena.16@est.ucab.edu.ve>
2020-05-06 23:58:34 -0400

    Se hicieron cambios en los loggers y los mensajes de error en el módulo de delivery_address. Se hicieron unos ajustes en la respuesta en el módulo de authentication

--- modules/delivery_address/delivery_address.model.js
+++ modules/delivery_address/delivery_address.model.js
@@ -9,8 +9,8 @@ module.exports = {
     } else if (req.originalUrl === "/bodenchufe/api/user/deliveryAddress") {
       return req.con
         .query(
-          `SELECT * FROM EC_DELIVERY_ADDRESS WHERE FK_USER_ID = ${req.user_id}
-           AND FK_STATUS_ID = (SELECT STATUS_ID FROM EC_STATUS WHERE STATUS_NAME = 'active')`
+          `SELECT * FROM EC_DELIVERY_ADDRESS WHERE fk_user_id = ${req.user_id}
+           AND fk_status_id = (SELECT status_id FROM EC_STATUS WHERE status_name = 'ACTIVE')`
         )
         .catch((error) => {
           return new Error(error);
@@ -20,10 +20,10 @@ module.exports = {
   updateAddressStatus: (req) => {
     return req.con
       .query(
-        `UPDATE EC_DELIVERY_ADDRESS SET FK_STATUS_ID = 
-        (SELECT STATUS_ID FROM EC_STATUS WHERE STATUS_NAME = '${req.body.status_name}')
-        WHERE FK_USER_ID = ${req.user_id}
-        AND DELIVERY_ADDRESS_ID = ${req.params.deliveryAddressId}`
+        `UPDATE EC_DELIVERY_ADDRESS SET fk_status_id = 
+        (SELECT status_id FROM EC_STATUS WHERE status_name = '${req.body.status_name}')
+        WHERE fk_user_id = ${req.user_id}
+        AND delivery_address_id = ${req.params.deliveryAddressId}`
       )
       .catch((error) => {
         return new Error(error);
