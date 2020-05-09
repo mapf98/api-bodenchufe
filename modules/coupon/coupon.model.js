fb4d9055e107a84734f7d66682978336cfe7d4bb
alexjose131 <ajfernandez.17@est.ucab.edu.ve>
2020-05-08 21:06:03 -0400

    cambios en el updateCoupon y addCoupon (se agrega minimo y maximo de precio para usar el cupon)

--- modules/coupon/coupon.model.js
+++ modules/coupon/coupon.model.js
@@ -7,8 +7,8 @@ module.exports = {
   addCoupon: (con, body) => {
     return con
       .query(
-        `INSERT INTO EC_COUPON (coupon_name, coupon_discount_rate, fk_status_id, fk_user_id) 
-         VALUES ('${body.coupon_name}', '${body.coupon_discount_rate}', (SELECT status_id FROM EC_STATUS WHERE status_name = 'ACTIVE') , ${body.fk_user_id})`
+        `INSERT INTO EC_COUPON (coupon_name, coupon_discount_rate, coupon_min_use, coupon_max_use, fk_status_id, fk_user_id) 
+         VALUES ('${body.coupon_name}', '${body.coupon_discount_rate}', ${body.coupon_min_use}, ${body.coupon_max_use}, (SELECT status_id FROM EC_STATUS WHERE status_name = 'ACTIVE') , ${body.fk_user_id})`
       )
       .catch((error) => {
         return new Error(error);
@@ -16,7 +16,7 @@ module.exports = {
   },
   updateCoupon: (con, body) => {
     return con.result(
-      `UPDATE EC_COUPON SET coupon_name = '${body.coupon_name}', coupon_discount_rate = '${body.coupon_discount_rate}' WHERE coupon_id = ${body.coupon_id}`
+      `UPDATE EC_COUPON SET coupon_name = '${body.coupon_name}', coupon_discount_rate = '${body.coupon_discount_rate}', coupon_min_use = ${body.coupon_min_use}, coupon_max_use = ${body.coupon_max_use} WHERE coupon_id = ${body.coupon_id}`
     );
   },
   disableCoupon: (con, body) => {
