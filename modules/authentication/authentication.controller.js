16e44b30a6d58a3385556a9f28aa9b56432d4293
Alexander.Fernandez <63027068+alexjose131@users.noreply.github.com>
2020-05-09 19:03:00 -0400

    Fase1 007 update (#49)
    * cambios en la ruta de user (deliveryAddress)
    
    * Se modificaron las respuestas en base a una nomenclatura común, se hicieron los loggers más específicos y los mensajes de error  en el módulo de categoría
    
    * Se modificaron las respuestas en base a una nomenclatura común, se hicieron los loggers más específicos y los mensajes de error en el módulo de coupon
    
    * Se arregló un error en el query de updateCoupon
    
    * Arreglos en disableCoupon
    
    * Se hicieron cambios en los loggers y los mensajes de error en el módulo de delivery_address. Se hicieron unos ajustes en la respuesta en el módulo de authentication
    
    * Se borró el módulo de language
    
    * Se hicieron cambios en los loggers y los mensajes de error en el módulo de offer
    
    * Se eliminó la referencia de language en el router.js
    
    * Se hicieron cambios en los loggers y los mensajes de error en el módulo de producto
    
    * Se agregó un cambio en product
    
    * Cambios en el logger y los mensajes de error en el módulo order
    
    * Se eliminó el módulo de qualification
    
    * Se hicieron cambios en los loggers y los mensajes de error en el módulo de provider
    
    * Cambios en el logger y mensajes de error en el módulo setting
    
    * Se agregaron los loggers y los mensajes de error asociados el módulo de user
    
    * se agregan cambios en los scripts de BD y Se agrega cupon al registrarse
    
    * cambios en el updateCoupon y addCoupon (se agrega minimo y maximo de precio para usar el cupon)
    
    * Nuevos inserts de BD con product y category
    
    * se agrega mas scripts de inserts a la base de datos
    
    * scripts de inserts actualizados
    
    * Se agregaron usuarios al usuario
    
    * scripts actualizados
    
    * Se agregaron inserts de cupones
    
    * Inserts para order
    
    Co-authored-by: Miguel Antonio Peña Fraga <mapena.16@est.ucab.edu.ve>

--- modules/authentication/authentication.controller.js
+++ modules/authentication/authentication.controller.js
@@ -74,6 +74,7 @@ module.exports = {
   },
   signUp: async (req, res, next) => {
     let result = await authenticationModel.signUp(req.con, req.body);
+
     if (result instanceof Error) {
       logger.error(
         "Error en el módulo authentication (POST /authentication/signUp - signUp())"
@@ -87,6 +88,10 @@ module.exports = {
       );
     }
 
+    let addCouponToUser = await authenticationModel.addWelcomeCoupon(
+      req.con,
+      result[0].user_id
+    );
     new Email(result[0]).sendWelcome();
 
     logger.info(
